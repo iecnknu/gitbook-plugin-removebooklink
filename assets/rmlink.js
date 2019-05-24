bfdbd1bc96943c433ceddcef0f02b889db1a6864
kuan-ju wang <dreamdomroy@gmail.com>
2019-05-24 15:34:39 +0800

    renew link

--- assets/rmlink.js
+++ assets/rmlink.js
@@ -2,17 +2,13 @@ require([
     'gitbook',
     'jquery'
 ], function (gitbook, $) {
-    function changeLink() {
+    function removelink() {
         var link = $('a.custom-link')
-        link.attr('href', 'https://www.nknucsec.com')
-        link.text('Published with IEC, NKNU')
-
-        var icon_link = $('link[rel=\'shortcut icon\']')
-        icon_link.attr('href', 'https://drive.google.com/uc?export=download&id=1hvAlY4gOjjzHm4XA-Wv64A1_dX9z0pk0')
+        link.remove()
     }
-    changeLink()
+    removelink()
     gitbook.events.on('page.change', function () {
-        changeLink()
+        removelink()
     })
 
 
