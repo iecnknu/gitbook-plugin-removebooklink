1c58f037959b933e1f1565d30eb6f9e6cb7d5d00
kuan-ju wang <dreamdomroy@gmail.com>
2019-05-24 15:44:53 +0800

    renew link

--- assets/rmlink.js
+++ assets/rmlink.js
@@ -4,7 +4,11 @@ require([
 ], function (gitbook, $) {
     function removelink() {
         var link = $('a.custom-link')
+        var divider = $('li.divider')
         link.remove()
+        divider.remove()
+        var searchText = $('#book-search-input input')
+        searchText.attr('placeholder','開始搜尋')
     }
     removelink()
     gitbook.events.on('page.change', function () {
